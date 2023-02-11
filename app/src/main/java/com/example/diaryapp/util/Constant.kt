cecc244ab466074cf97889d1466e304c3c70b292
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-02-11 18:20:32 +0900

    feat: mongo DB setup
    mongo db, realm 라이브러리 의존성 추가, 구글 로그인 연동

---
+++ app/src/main/java/com/example/diaryapp/util/Constant.kt
@@ -0,0 +1,8 @@
+package com.example.diaryapp.util
+
+object Constant {
+
+    const val APP_ID = "mydiaryapp-fyxoh"
+    const val CLIENT_ID = "489645980044-lrg9of14con3elbs212srsu3d0s83t5t.apps.googleusercontent.com"
+
+}
