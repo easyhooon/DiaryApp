0f736a04dcbfd8384b5ae72a4c422b600efcc7d6
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-02-16 14:31:01 +0900

    design: show Gallery spring bouncy Animation 적용

--- app/src/main/java/com/example/diaryapp/presentation/components/DiaryHolder.kt
+++ app/src/main/java/com/example/diaryapp/presentation/components/DiaryHolder.kt
@@ -1,6 +1,10 @@
 package com.example.diaryapp.presentation.components
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -82,7 +86,16 @@ fun DiaryHolder(
                 }
                 // animatedVisibility composable function will be visible
                 // only if gallery is opened
-                AnimatedVisibility(visible = galleryOpened) {
+                // add optional spring bouncy animation
+                AnimatedVisibility(
+                    visible = galleryOpened,
+                    enter = fadeIn() + expandVertically(
+                        animationSpec = spring(
+                            dampingRatio = Spring.DampingRatioMediumBouncy,
+                            stiffness = Spring.StiffnessLow
+                        )
+                    )
+                ) {
                     Column(modifier = Modifier.padding(all = 14.dp)) {
                         Gallery(images = diary.images)
                     }
